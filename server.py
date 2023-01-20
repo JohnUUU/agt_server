9e63b33fc5a10f3ba370ba3990c10e6ba76db82c
dipplestix <chrism315@gmail.com>
2023-01-20 09:44:42 -0500

    Fixed server.py

--- server.py
+++ server.py
@@ -10,12 +10,12 @@ actions = {0: [None]*1000, 1: [None]*1000}
 # A function to determine the winner of a round
 def determine_winner(action1, action2):
     if action1 == action2:
-        return 0
+        return 3
     elif (action1 == 'rock' and action2 == 'scissors') or (action1 == 'paper' and action2 == 'rock') or (
             action1 == 'scissors' and action2 == 'paper'):
-        return 1
+        return 0
     else:
-        return 2
+        return 1
 
 
 # A function to handle a single client
@@ -25,24 +25,32 @@ def handle_client(client, player_num):
         opp = 1
     else:
         opp = 0
-    while rounds_played < 1:
+    while rounds_played < 5:
         time.sleep(.2)
         # Wait for the agent to send its action
+        print(f'I am here for player {player_num}')
         actions[player_num][rounds_played] = client.recv(1024).decode()
-
+        print(actions[player_num][rounds_played])
         # Check if both agents have sent their actions
         while True:
             if actions[0][rounds_played] is not None and actions[1][rounds_played] is not None:
                 a1 = actions[0][rounds_played]
                 a2 = actions[1][rounds_played]
-                winner = determine_winner(actions[0], actions[1])
+                winner = determine_winner(a1, a2)
+                print(f'The winner is {winner}')
+                if winner == player_num:
+                    util = 1
+                elif winner == 3:
+                    util = 0
+                else:
+                    util = -1
                 if player_num == 0:
                     print(f'It is round {rounds_played}. Player 1 played {a1} and player 2 played {a2}. '
                       f'Player {winner} won')
                 print(actions[opp][rounds_played], winner)
-                client.send(f'{actions[opp][rounds_played]}, {winner}'.encode())
-                actions[player_num] = None
+                client.send(f'{actions[opp][rounds_played]}, {util}'.encode())
                 rounds_played += 1
+                break
     print(f'game over for player {player_num}')
     client.send('Game Over'.encode())
     client.close()
