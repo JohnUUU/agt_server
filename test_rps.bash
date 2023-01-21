c1d8dbb31bbf0436043f3b112a8d135e5796ab58
Chris Mascioli <chrism315@gmail.com>
2023-01-21 15:25:49 -0500

    Update test_rps.bash

--- test_rps.bash
+++ test_rps.bash
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]; then
-    echo "Please provide number of terminals as an argument"
-    exit 1
+            echo "Please provide number of terminals as an argument"
+                exit 1
 fi
 
 for i in $(seq 1 $1)
 do
-  bash -c "python random_agent.py" &
+          bash -c "python random_agent.py" &
 done
+wait
