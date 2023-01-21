07ed4005af52eeb99554e5d7b2cd96c50c27c22e
John Wu <john_w_wu@brown.edu>
2023-01-21 16:30:07 -0500

    Reset Server.py

--- server.py
+++ server.py
@@ -17,21 +17,10 @@ def determine_winner(action1, action2):
 class Server:
     def __init__(self, n_players):
         self.n_players = n_players
-        self.actions = {i: [None]*1000000 for i in range(n_players)}
+        self.actions = {i: [None]*1000 for i in range(n_players)}
         self.wins = {i: 0 for i in range(n_players)}
         self.wins[3] = 0
         self.clients = defaultdict(lambda: None)
-        self.in_progress = True
-        self.message = {p: None for p in range(n_players)}
-
-    def get_initial_message(self):
-        raise NotImplementedError
-
-    def run_game(self):
-        raise NotImplementedError
-
-    def create_pairings(self):
-        raise NotImplementedError
 
     def start(self):
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -48,59 +37,53 @@ class Server:
             print(f'Player {i} connected from {address} named {name}')
             # Send what player number they are
             client.send(str(i).encode())
-            time.sleep(.002)
-
-            initial_message = self.get_initial_message()
-            client.send(initial_message.encode())
+            time.sleep(0.2)
+            # Send an initial message, in this case it is the game we are playing. It can be a valuation or something instead
+            client.send('RPS'.encode())
             self.clients[i] = client
             # Start the thread
-            threading.Thread(target=self.handle_client, args=(client, i)).start()
-        print(f'I have {self.n_players} agents connected and I am starting the game')
-        self.run_game()
+            threading.Thread(target=self.handle_client,
+                             args=(client, i)).start()
+        print('I have 2 agents connected')
 
-    # def handle_client(self, client, player_num):
-    #     rounds_played = 0
-    #     if player_num == 0:
-    #         opp = 1
-    #     else:
-    #         opp = 0
-    #     time.sleep(.002)
-    #     while rounds_played < 5:
-    #         # Wait for the agent to send its action
-    #         self.actions[player_num][rounds_played] = client.recv(1024).decode()
-    #         # Check if both agents have sent their actions
-    #         while True:
-    #             if self.actions[0][rounds_played] is not None and self.actions[1][rounds_played] is not None:
-    #                 a1 = self.actions[0][rounds_played]
-    #                 a2 = self.actions[1][rounds_played]
-    #                 winner = determine_winner(a1, a2)
-    #                 if winner == player_num:
-    #                     util = 1.
-    #                 elif winner == 3:
-    #                     util = 0.
-    #                 else:
-    #                     util = -1.
-    #                 if player_num == 0:
-    #                     # print(f'It is round {rounds_played}. Player 0 played {a1} and player 1 played {a2}.')
-    #                     self.wins[winner] += 1
-    #                 client.send(f'{[self.actions[opp][rounds_played]]}, {util}'.encode())
-    #                 rounds_played += 1
-    #                 break
-    #     client.send('Game Over'.encode())
-    #     if player_num == 0:
-    #         print(f'Player 0 won {self.wins[0]} times, player 1 won {self.wins[1]} time, '
-    #               f'and there was {self.wins[3]} draws')
-    #
-    #     client.close()
     def handle_client(self, client, player_num):
         rounds_played = 0
-        while self.in_progress:
+        if player_num == 0:
+            opp = 1
+        else:
+            opp = 0
+        while rounds_played < 5:
+            time.sleep(.2)
             # Wait for the agent to send its action
-            self.actions[player_num][rounds_played] = client.recv(1024).decode()
-            rounds_played += 1
-            while self.message[player_num] is None:
-                pass
-            client.send(self.message[player_num].encode())
-            self.message[player_num] = None
-        # client.send("Game Over".encode())
+            self.actions[player_num][rounds_played] = client.recv(
+                1024).decode()
+            # Check if both agents have sent their actions
+            while True:
+                if self.actions[0][rounds_played] is not None and self.actions[1][rounds_played] is not None:
+                    a1 = self.actions[0][rounds_played]
+                    a2 = self.actions[1][rounds_played]
+                    winner = determine_winner(a1, a2)
+                    if winner == player_num:
+                        util = 1.
+                    elif winner == 3:
+                        util = 0.
+                    else:
+                        util = -1.
+                    if player_num == 0:
+                        print(
+                            f'It is round {rounds_played}. Player 0 played {a1} and player 1 played {a2}.')
+                        self.wins[winner] += 1
+                    client.send(
+                        f'{[self.actions[opp][rounds_played]]}, {util}'.encode())
+                    rounds_played += 1
+                    break
+        client.send('Game Over'.encode())
+        if player_num == 0:
+            print(f'Player 0 won {self.wins[0]} times, player 1 won {self.wins[1]} time, '
+                  f'and there was {self.wins[3]} draws')
+
         client.close()
+
+
+server = Server(2)
+server.start()
